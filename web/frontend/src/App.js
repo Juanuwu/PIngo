969f9fff13d9e712b4717da693a7ecfa1cb28071
Juan <juan.carosella@gmail.com>
2021-11-20 17:51:24 -0300

    parcial

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,21 +1,64 @@
 import React, { useState } from "react";
 import './index';
+import axios from "axios";
+const MyApp = {};
 
-function App(props) {
+function getRQ() {
+    axios.get('/api/todos').then(function (result) {
 
 
-    const [tasks] = useState(props.tasks);
+        console.log(result.data)
+        MyApp.data = result.data
+    });
+}
+getRQ()
+function App() {
+
     var numero = 0
     numero = numero + 1
+
     return (
         <div className="col"> 
-            {
-            tasks.map(task => 
+            {MyApp.data.map(task =>
                 
-            <div class="chain">{"Nombre:\n" + task.data + "\n" + "Hash:" + task._id + "\n"+ "Prev:" + task.prev + "\n"+" Tipo: " + numero}  <hr className="dashed"/> </div>)
+            <div class="events">{"Nombre:\n" + task.data + "\n" + "Hash:" + task._id + "\n"+ "Prev:" + task.prev + "\n"+" Tipo: " + numero}  <hr className="dashed"/> </div>)
             }
         </div>
     );
 
 }
+
+function selectElement(selector){
+    return document.querySelector(selector);
+}
+//Limpiar el contenido
+function clearResult(){
+    selectElement('.search-results').innerHTML="";
+}
+
+function getResults(){
+    const search = selectElement('.searchbar').value;
+    clearResult();
+    if(search.length > 0){
+
+        MyApp.data.map(task =>{
+        if (
+            task.data.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+            task.data.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+            ){
+                selectElement('.search-results').innerHTML+=`
+                <div class = "search-results-item">
+                    <span class= "search-item">${task.data}</span>
+                    <span class= "search-item">${task.data}</span>
+                    <span class= "search-item">${task.data}</span>
+                </div>
+                `;
+            }
+        });
+    }
+
+}
+
+selectElement('.searchbar').addEventListener('keyup', getResults);
+
     export default App;
--- web/frontend/src/index.js
+++ web/frontend/src/index.js
@@ -5,20 +5,5 @@ import App from './App';
 import axios from "axios";
 
 
-function getRQ() {
-    axios.get('/api/todos').then(function (result) {
-
-        doSomething(result.data);
-        console.log(result.data)
-    });
-}
-
-function doSomething (data) {
-    ReactDOM.render(<App tasks={data} />, document.getElementById("root"));
-}
-
-getRQ()
-
-
 
 
