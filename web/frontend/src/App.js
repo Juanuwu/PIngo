6e84e9969fa4129bb7e62aa101caffed10a4b59e
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-11-20 14:14:53 -0300

    pagina

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
-import './App.css';
+import './index';
 
 function App(props) {
 
 
     const [tasks] = useState(props.tasks);
-
+    var numero = 0
+    
     return (
-        <div className="col">
-            <h1>pingo</h1>
-            <p>luis pingo</p>
-            {tasks.map(task => <div className="chain">{"Nombre:" + task.data + "\n" + "Hash:" + task._id + "\n"+ "Prev:" + task.prev} <hr className="dashed"/> </div>)}
-
-
+        <div className="col"> numero = numero + 1
+            {
+            tasks.map(task => 
+                
+            <div class="chain">{"Nombre:" + task.data + "\n" + "Hash:" + task._id + "\n"+ "Prev:" + task.prev + "\n"+" Tipo: " + numero}  <hr className="dashed"/> </div>)
+            }
         </div>
     );
 
 }
-
     export default App;
