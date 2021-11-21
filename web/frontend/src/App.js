90238745ad207922d30fcb67fae52e890f180867
Fabo xw <fabianmartinezrincon.123@gmail.com>
2021-11-21 17:43:39 -0300

    no teques css Juan

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -13,7 +13,7 @@ function App(props) {
         <div className="col">
             {film = MyApp.data.slice(0, 10).map((task) =>
 
-            <div class="chain">{task.data + "\n" + "Hash:"  + task._id + "\n"+ "Prev:" + task.prev + "\n"+" Tipo: " + numero}  <hr className="dashed"/> </div>)
+            <div class="chain">{task.data + "\n" + "Hash:"  + task._id + "\n"+ "Prev:" + task.prev }  <hr className="dashed"/> </div>)
             }
         </div>
     );
