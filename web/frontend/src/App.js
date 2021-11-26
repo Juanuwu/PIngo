4af9d9a51a216e904ec32b65bb37d7f56c8a20de
Juan <juan.carosella@gmail.com>
2021-11-26 14:55:22 -0300

    style

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -31,7 +31,7 @@ function App(props) {
                 <Draggable>
                     <div className="form-popup" id="myForm">
 
-                        <form onSubmit={handleSubmit} action="/action_page.php" className="form-container">
+                        <form onSubmit={handleSubmit} action="/action_page.php" className="form-container" autocomplete="off">
                             <h1>registra tu enefete de pingo de luis de pingo</h1>
 
                             <label form="email"><b>Nombre</b></label>
@@ -65,7 +65,7 @@ function App(props) {
 
                     <div className="form-popup" id="myForm">
 
-                        <form onSubmit={handleSubmit} action="/action_page.php" className="form-container">
+                        <form onSubmit={handleSubmit} action="/action_page.php" className="form-container" autocomplete="off">
                             <h1>registra tu enefete de pingo de luis de pingo</h1>
 
                             <label form="email"><b>Nombre</b></label>
@@ -80,7 +80,7 @@ function App(props) {
                         </form>
 
                     </div>
-                
+
             ]
         );
     }
